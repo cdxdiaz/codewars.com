9e51515d882a042e9b49f1adbf51dc01a72dc98b
cdxdiaz <wenai.sawatwathin@gmail.com>
2024-08-19 02:57:48 +0700

    Add greet.c

---
+++ 8kyu/greet.c
@@ -0,0 +1,20 @@
+/*
+
+Function 1 - hello world
+
+Make a simple function called greet that returns the most-famous "hello world!".
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+*/
+
+#include <stdio.h>
+
+const char* greet(){
+    return ("hello world!");
+}
+
+int main(void){
+    printf("%s\n", greet());
+    return (0);
+}
